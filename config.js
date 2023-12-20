c18577fc89da53875e8e5ccaf62097c3338a65ca
Tien Le <lvdtienpro13@gmail.com>
2023-12-20 22:57:12 +0700

    fist commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Phươngg Quỳnhh';
+const giftUrl = 'https://www.facebook.com/levietdaitien';
+const eventName = 'Chúc mừng sinh nhậtt';
+const titleCard = 'Tặng em <3';
+const contentCard = 'Xin chào người đặc biệt nhân một ngày đặc biệt. Hôm nay, lại một chương nữa trong cuốn sách cuộc đời chúng ta lại được viết, nhưng lần này, nó sẽ là một chương đặc biệt đẹp và tràn ngập niềm hạnh phúc. Chúc mừng sinh nhật em, chúc em tuổi mới ngày càng xinh đẹp, thông minh, chúc cho những tâm nguyện của em sớm thành hiện thực. Sanh thần vui vẻ, bbii 🎉💖';
+
+const giftImage = 'PQ.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
